93bdc1264486158c6943d798b5c916c57b529110
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2021-01-17 07:38:59 -0800

    Update screen.py

--- Dwindle/modules/screen.py
+++ Dwindle/modules/screen.py
@@ -18,19 +18,19 @@ def screen(update, context):
         status_code = 200
     except requests.ConnectionError as exception:
         status_code = 404
-
     if status_code == 200:
+
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome("chromedriver.exe",chrome_options=chrome_options)
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         driver.get(context.args[0])
         sleep(1)
         driver.get_screenshot_as_file("screen.png")
         driver.quit()
         bot.send_photo(chat_id=chat_id, photo=open('screen.png', 'rb'))
-
     else:
-        bot.send_photo(chat_id=chat_id, photo=open('Dwindle\drawable\\404_error.png','rb'),caption="OOPS! Web page not Found"+(emoji.emojize(":confused:",use_aliases=True)))
+        bot.send_photo(chat_id=chat_id, photo=open('Dwindle/drawable/404_error.png','rb'),caption="OOPS! Web page not Found"+(emoji.emojize(":confused:",use_aliases=True)))
 
