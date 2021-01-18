136752a6894f919e4ef3e87832bde6b12c497345
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2021-01-18 07:09:48 -0800

    Update sample-config.py

--- Dwindle/sample-config.py
+++ Dwindle/sample-config.py
@@ -5,10 +5,14 @@ if not __name__.endswith("sample_config"):
     quit(1)
 
 # Create a new config.py file in same dir and import, then extend this class.
+class Config(object):
+    # REQUIRED
+  TOKEN = "" # Your bot Token here
+    #Recommended
+  Webhook = False #Set Webhook
+  PORT = 8443 # Ports currently supported for Webhooks: 443, 80, 88, 8443.
 
-TOKEN = "" # Your bot Token here
-Webhook = "" #webhook url here.(Ex: https://dwindle.heroku.app.If you are using heroku get using the format - https://{your heroku app name here}.herokuapp.com/)
-GpApi = "" #Get this at https://bitly.is/accesstoken
-GpBase = " https://gplinks.in/api?api={}&url=".format(GpApi)
-bitlyapi = "" #Get this at https://bitly.is/accesstoken
-bitlybase = "https://api-ssl.bitly.com/v3/shorten?access_token={}&uri=".format(bitlyapi)
+  GpApi = "" #Get this at https://bitly.is/accesstoken
+  GpBase = " https://gplinks.in/api?api={}&url=".format(GpApi)
+  bitlyapi = "" #Get this at https://bitly.is/accesstoken
+  bitlybase = "https://api-ssl.bitly.com/v3/shorten?access_token={}&uri=".format(bitlyapi)
