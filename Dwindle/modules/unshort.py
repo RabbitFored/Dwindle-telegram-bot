ca7b425c33e36559f673732cebfa490b874c71ff
theostrich <79783371+theostrich@users.noreply.github.com>
2021-03-24 02:30:23 -0700

    Update unshort.py

--- Dwindle/modules/unshort.py
+++ Dwindle/modules/unshort.py
@@ -1,6 +1,9 @@
 import requests
 
 def unshort(update, context):
-    session = requests.Session()
-    unshortened = session.head(context.args[0], allow_redirects=True)
-    update.message.reply_text(unshortened.url)
+    if len(context.args)==1:
+        session = requests.Session()
+        unshortened = session.head(context.args[0], allow_redirects=True)
+        update.message.reply_text(unshortened.url)
+    else:
+        update.message.reply_text('Please provide some URL.')
