138b7358c4c1e1b620b03a6da64215ec050f8f5a
theostrich <79783371+theostrich@users.noreply.github.com>
2021-03-25 23:29:43 -0700

    Update sample-config.py

--- Dwindle/sample-config.py
+++ Dwindle/sample-config.py
@@ -2,7 +2,7 @@ if not __name__.endswith("sample_config"):
     import sys
     print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
           "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
-    quit(1)
+    sys.exit(1)
 
 # Create a new config.py file in same dir and import, then extend this class.
 
