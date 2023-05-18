9cc83fb646a83c1186dad929b2d3358d4e3e1f72
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2023-05-18 12:25:05 +0530

    Update __main__.py

--- Dwindle/__main__.py
+++ Dwindle/__main__.py
@@ -75,17 +75,10 @@ def button(update, context):
     query.edit_message_text(text="Support Group arrives Soon")
 
 def donate(update, context):
-    keyboard = [
-        [
-            telegram.InlineKeyboardButton("Contribute",
-                                          url="https://github.com/RabbitFored"),
-            telegram.InlineKeyboardButton("Paypal Us",url="https://paypal.me/donateostrich"),
-        ],
-    ]
-
-    reply_markup = telegram.InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Thank you for your wish to contribute. I hope you enjoyed using our services. Make a small donation/contribute to let this project alive." , reply_markup=reply_markup)
-
+  update.message.reply_text("Thank you for your wish to contribute. I hope you enjoyed using our services. Make a small donation/contribution to let this project stay alive.",
+                           reply_markup=telegram.InlineKeyboardMarkup([
+                                          [ telegram.InlineKeyboardButton("Contribute", url="https://github.com/theostrich/Dwindle-telegram-bot"),
+                                            telegram.InlineKeyboardButton("Paypal Us", url="https://paypal.me/donateostrich") ]]))
 def button(update, context):
     query = update.callback_query
     query.answer()
