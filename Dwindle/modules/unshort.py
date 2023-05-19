8f6eb9f4933aa811b9360c8d59ef7a6a2f2fa09f
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2023-05-19 13:55:15 +0530

    Update unshort.py
    Update unshort.py

--- Dwindle/modules/unshort.py
+++ Dwindle/modules/unshort.py
@@ -1,9 +1,27 @@
 import requests
 
+unshortmessage = "<b>Your URL : </b> {} \n\n<b>Expanded URL :</b> \n\n<b> - </b> {}"
+'''       '''
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+  
 def unshort(update, context):
+      
+    if bool(update.message):
+       message = update.message
+       text = remove_prefix(update.message.text , "/unshort ")
+    if bool(update.callback_query):
+       message = update.callback_query.message
+       text = update.callback_query.message.reply_to_message.text
+      
+    context.args = text.split(" ")
+
     if len(context.args)==1:
         session = requests.Session()
         unshortened = session.head(context.args[0], allow_redirects=True)
-        update.message.reply_text(unshortened.url)
+        message.reply_text(unshortmessage.format(context.args[0],unshortened.url),parse_mode='html')
     else:
-        update.message.reply_text('Please provide some URL.')
+        message.reply_text('Please provide me some URL.')
